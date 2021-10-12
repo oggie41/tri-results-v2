7c80d536162cd0751584f187f5a82649e9862bda
David Ogle <dogle@bartecmunicipal.com>
2021-10-12 17:35:11 +0100

    Table row striping now correct after filtering

--- TriResultsV2/wwwroot/js/site.js
+++ TriResultsV2/wwwroot/js/site.js
@@ -22,4 +22,17 @@ function courseChanged(obj, parentId) {
         $("#" + parentId + " [data-course]").hide();
         $("#" + parentId + " [data-course='" + obj.value + "']").fadeIn();
     }
+
+    // If the parent element is a table re-apply the table striping after filtering.
+    var elementType = $("#" + parentId).get(0).tagName;
+
+    if (elementType.toUpperCase() === "TABLE") {
+        $("#" + parentId + " tr").each(function(index) {
+            $(this).removeClass("tri-striped-row");
+        });
+
+        $("#" + parentId + " tr:visible").each(function(index) {
+            $(this).toggleClass("tri-striped-row", !!(index & 1));
+        });
+    }
 };
