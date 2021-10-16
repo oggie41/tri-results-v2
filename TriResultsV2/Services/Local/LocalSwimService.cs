0ae92d758dbac99ab69fd27cbe4d095f71c9948a
David Ogle <dogle@bartecmunicipal.com>
2021-10-16 09:46:15 +0100

    Past swim time trial results added

--- TriResultsV2/Services/Local/LocalSwimService.cs
+++ TriResultsV2/Services/Local/LocalSwimService.cs
@@ -14,7 +14,7 @@ namespace TriResultsV2.Services.Local
         {
             var eventResults = new List<EventResult>();
 
-            var swimResult3221940710 = new EventResult
+            var result = new EventResult
             {
                 Id = 3221940710,
                 GarminId = 3221940710,
@@ -26,7 +26,61 @@ namespace TriResultsV2.Services.Local
                 TotalTime = new TimeSpan(0, 3, 18),
                 PersonalBest = true
             };
-            eventResults.Add(swimResult3221940710);
+            // TODO: Add Swim CSS calculation. See http://previous.swimsmooth.com/training.html.
+            //result.AddEventFigure("CSS: 1:47/100m");
+            eventResults.Add(result);
+
+            result = new EventResult
+            {
+                Id = 1647266045,
+                GarminId = 1647266045,
+                Sport = SportType.Swim,
+                Distance = 200,
+                DistanceUnit = DistanceUnit.Metres,
+                EventDate = new DateTime(2017, 3, 29),
+                EventName = "Westfield 200m TT",
+                TotalTime = new TimeSpan(0, 3, 21)
+            };
+            eventResults.Add(result);
+
+            result = new EventResult
+            {
+                Id = 1586996421,
+                GarminId = 1586996421,
+                Sport = SportType.Swim,
+                Distance = 200,
+                DistanceUnit = DistanceUnit.Metres,
+                EventDate = new DateTime(2017, 2, 23),
+                EventName = "Westfield 200m TT",
+                TotalTime = new TimeSpan(0, 3, 26)
+            };
+            eventResults.Add(result);
+
+            result = new EventResult
+            {
+                Id = 984904552,
+                GarminId = 984904552,
+                Sport = SportType.Swim,
+                Distance = 200,
+                DistanceUnit = DistanceUnit.Metres,
+                EventDate = new DateTime(2015, 12, 16),
+                EventName = "Westfield 200m TT",
+                TotalTime = new TimeSpan(0, 3, 29)
+            };
+            eventResults.Add(result);
+
+            result = new EventResult
+            {
+                Id = 940902079,
+                GarminId = 940902079,
+                Sport = SportType.Swim,
+                Distance = 200,
+                DistanceUnit = DistanceUnit.Metres,
+                EventDate = new DateTime(2015, 10, 28),
+                EventName = "Westfield 200m TT",
+                TotalTime = new TimeSpan(0, 3, 29)
+            };
+            eventResults.Add(result);
 
             return eventResults;
         }
@@ -35,7 +89,7 @@ namespace TriResultsV2.Services.Local
         {
             var eventResults = new List<EventResult>();
 
-            var swimResult3221940710 = new EventResult
+            var result = new EventResult
             {
                 Id = 3221940710,
                 GarminId = 3221940710,
@@ -47,7 +101,60 @@ namespace TriResultsV2.Services.Local
                 TotalTime = new TimeSpan(0, 6, 52),
                 PersonalBest = true
             };
-            eventResults.Add(swimResult3221940710);
+            //result.AddEventFigure("CSS: 1:47/100m");
+            eventResults.Add(result);
+
+            result = new EventResult
+            {
+                Id = 1647266045,
+                GarminId = 1647266045,
+                Sport = SportType.Swim,
+                Distance = 400,
+                DistanceUnit = DistanceUnit.Metres,
+                EventDate = new DateTime(2017, 3, 29),
+                EventName = "Westfield 400m TT",
+                TotalTime = new TimeSpan(0, 6, 56)
+            };
+            eventResults.Add(result);
+
+            result = new EventResult
+            {
+                Id = 1586996421,
+                GarminId = 1586996421,
+                Sport = SportType.Swim,
+                Distance = 400,
+                DistanceUnit = DistanceUnit.Metres,
+                EventDate = new DateTime(2017, 2, 23),
+                EventName = "Westfield 400m TT",
+                TotalTime = new TimeSpan(0, 7, 23)
+            };
+            eventResults.Add(result);
+
+            result = new EventResult
+            {
+                Id = 984904552,
+                GarminId = 984904552,
+                Sport = SportType.Swim,
+                Distance = 400,
+                DistanceUnit = DistanceUnit.Metres,
+                EventDate = new DateTime(2015, 12, 16),
+                EventName = "Westfield 400m TT",
+                TotalTime = new TimeSpan(0, 7, 25)
+            };
+            eventResults.Add(result);
+
+            result = new EventResult
+            {
+                Id = 940902079,
+                GarminId = 940902079,
+                Sport = SportType.Swim,
+                Distance = 400,
+                DistanceUnit = DistanceUnit.Metres,
+                EventDate = new DateTime(2015, 10, 28),
+                EventName = "Westfield 400m TT",
+                TotalTime = new TimeSpan(0, 7, 21)
+            };
+            eventResults.Add(result);
 
             return eventResults;
         }
