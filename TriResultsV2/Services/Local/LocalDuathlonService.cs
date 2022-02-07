5304ff2f4d69a436b9d5cdd2e217087c867af9f8
David Ogle <dogle@bartecmunicipal.com>
2022-02-07 16:31:35 +0000

    Added duathlon official results URLs

--- TriResultsV2/Services/Local/LocalDuathlonService.cs
+++ TriResultsV2/Services/Local/LocalDuathlonService.cs
@@ -27,18 +27,18 @@ namespace TriResultsV2.Services.Local
                 Discipline2DistanceUnit = DistanceUnit.Kilometres,
                 Discipline3Distance = 2.5,
                 Discipline3DistanceUnit = DistanceUnit.Kilometres,
-                Discipline1Time = new TimeSpan(0, 20, 21),
+                Discipline1Time = new TimeSpan(0, 20, 18),
                 T1Time = new TimeSpan(0, 1, 38),
-                Discipline2Time = new TimeSpan(0, 36, 29),
-                T2Time = new TimeSpan(0, 1, 9),
-                Discipline3Time = new TimeSpan(0, 10, 15),
+                Discipline2Time = new TimeSpan(0, 36, 30),
+                T2Time = new TimeSpan(0, 1, 10),
+                Discipline3Time = new TimeSpan(0, 10, 19),
                 TotalTime = new TimeSpan(1, 9, 55),
                 Discipline1AvgRunPaceMinMi = new TimeSpan(0, 6, 43),
                 AvgBikeSpeedMph = 19.4,
                 Discipline3AvgRunPaceMinMi = new TimeSpan(0, 7, 2),
-                TotalParticipants = 251,
+                TotalParticipants = 253,
                 Discipline1Position = 43,
-                T1Position = 67,
+                T1Position = 66,
                 Discipline2Position = 53,
                 T2Position = 65,
                 Discipline3Position = 45,
@@ -46,6 +46,7 @@ namespace TriResultsV2.Services.Local
                 Weather = WeatherType.SunnyCloudy,
                 TemperatureDegreesCelsius = 9,
                 WindMph = 12,
+                OfficialResultsUrl = "https://www.resultsbase.net/event/4649/results?round=10657",
                 EventNotes = "Good weather, enjoyed it again this year. Pleased to knock almost a minute off from last year.<br/>" +
                              "Felt really good on the first run. Went slightly easier on the bike than last year to save my legs for the second run. Think there's still room for improvement on the bike though; hoping to take some time off the bike leg next year.<br/>" +
                              "Second run was way better than last year (better pacing on the bike and better strength endurance from the gym work).<br/>" +
@@ -76,16 +77,17 @@ namespace TriResultsV2.Services.Local
                 Discipline1AvgRunPaceMinMi = new TimeSpan(0, 6, 44),
                 AvgBikeSpeedMph = 19.8,
                 Discipline3AvgRunPaceMinMi = new TimeSpan(0, 7, 26),
-                TotalParticipants = 324,
-                Discipline1Position = 99,
-                T1Position = 147,
-                Discipline2Position = 107,
-                T2Position = 140,
-                Discipline3Position = 127,
-                OverallPosition = 103,
+                TotalParticipants = 325,
+                Discipline1Position = 92,
+                T1Position = 110,
+                Discipline2Position = 98,
+                T2Position = 113,
+                Discipline3Position = 108,
+                OverallPosition = 104,
                 Weather = WeatherType.Sunny,
                 TemperatureDegreesCelsius = 15,
                 WindMph = 6,
+                OfficialResultsUrl = "https://resultsbase.net/event/4146/results?round=9195",
                 EventNotes = "Good course, enjoyed the race. It was tough in the heat though.<br/>" +
                              "The first run was steady enough, although a bit more undulating than I expected.<br/>" +
                              "The bike leg went well; feeling stronger on the bike after all the winter turbo.<br/>" +
@@ -127,6 +129,7 @@ namespace TriResultsV2.Services.Local
                 Weather = WeatherType.Rainy,
                 TemperatureDegreesCelsius = 4,
                 WindMph = 12,
+                OfficialResultsUrl = "https://www.stuweb.co.uk/race/1si/",
                 EventNotes = "First standard distance duathlon completed. Really tough race with the conditions.<br/>" +
                  "First run went well. Bad calf cramps on the bike, but managed to keep going; run -> bike brick sessions needed to help with this.<br/>" +
                  "Like a muppet forgot to take my helmet off in T2, so had to run with it! Second run was really tough - couldn't feel my feet!"
@@ -166,6 +169,7 @@ namespace TriResultsV2.Services.Local
                 Weather = WeatherType.Cloudy,
                 TemperatureDegreesCelsius = 9,
                 WindMph = 8,
+                OfficialResultsUrl = "https://www.osbevents.com/results/", //http://dload.osb.s3.amazonaws.com/results/clumberoffroad15results.xls
                 EventNotes = "Tough event. Felt good on the runs, but the cycle leg was really tough, especially on the hybrid (borrowed from Eddy)."
             };
             eventResults.Add(result);
