ecf2c0a1ab3e371dc1b16bb4eca6b843564e22f2
David Ogle <dogle@bartecmunicipal.com>
2022-03-02 07:43:00 +0000

    Track Builder mock-up

---
+++ TriResultsV2/Pages/TrackBuilderMockup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TriResultsV2.Pages
+{
+    public class TrackBuilderMockupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- TriResultsV2/wwwroot/js/site.js
+++ TriResultsV2/wwwroot/js/site.js
@@ -5,6 +5,10 @@
 /* Using a JavaScript closure - it creates an Immediately-Invoked Function Expression (IIFE), allowing for private variables/functions. */
 var Tri = (function () {
     /* Private Functions/Variables */
+    var trackConnectorCount = 3;
+    var activeTrackConnector = 3;
+    var traversalAdded = false;
+
     function reapplyTableStriping(parentId) {
         $("#" + parentId + " tr").each(function (index) {
             $(this).removeClass("tri-striped-row");
@@ -13,6 +17,38 @@ var Tri = (function () {
         $("#" + parentId + " tr:visible").each(function (index) {
             $(this).toggleClass("tri-striped-row", !!(index & 1));
         });
+    }
+
+    function changeActiveTrackConnector() {
+        for (let i = 0; i < trackConnectorCount; i++) {
+            $("#icoActiveTrackConnector").removeClass("fa-circle-" + (i+1));
+        }
+        $("#icoActiveTrackConnector").addClass("fa-circle-" + activeTrackConnector);
+
+        $(".track-connector-icon").removeClass("text-success");
+        $(".track-connector-icon").removeClass("text-danger");
+        $(".track-connector-icon:not(.fa-circle-" + activeTrackConnector + ")").addClass("text-danger");
+        $(".track-connector-icon.fa-circle-" + activeTrackConnector).addClass("text-success");
+
+        if (activeTrackConnector === 1) {
+            $("#btnTrackConnectorDelete").prop("disabled", true);
+        } else {
+            $("#btnTrackConnectorDelete").prop("disabled", false);
+        }
+
+        var lastTrackConnectorInList = 3;
+
+        if (traversalAdded) {
+            lastTrackConnectorInList = 2;
+        }
+
+        if (activeTrackConnector === lastTrackConnectorInList) {
+            $("#divAddTraversalValid").show();
+            $("#divAddTraversalInvalid").hide();
+        } else {
+            $("#divAddTraversalValid").hide();
+            $("#divAddTraversalInvalid").show();
+        }
     }
     /* End Private Functions/Variables */
 
@@ -82,6 +118,110 @@ var Tri = (function () {
             if (elementType.toUpperCase() === "TABLE") {
                 reapplyTableStriping(parentId);
             }
+        },
+
+        InitialiseTrackBuilder: function () {
+            $(function () {
+                var slOffset = document.getElementById("slOffset");
+
+                if (slOffset !== null && slOffset !== undefined) {
+                    var lblSliderValue = document.getElementById("lblOffsetValue");
+
+                    if (lblSliderValue !== null && lblSliderValue !== undefined) {
+                        slOffset.oninput = function () {
+                            var offsetValue = this.value;
+                            var offsetDirection = "";
+                            var offsetDescription = "Shared single road in both directions";
+
+                            if (offsetValue > 0) {
+                                offsetDirection = "right";
+                            }
+
+                            if (offsetValue < 0) {
+                                offsetDirection = "left";
+                                offsetValue = offsetValue * -1;
+                            }
+
+                            if (offsetValue > 0) {
+                                offsetDescription = "Opposite direction as adjacent road (with overlap)";
+
+                                if (offsetValue == 3) {
+                                    offsetDescription = "Opposite direction as adjacent road (no spacing)";
+                                } else if (offsetValue > 3) {
+                                    offsetDescription = "Opposite direction as adjacent road (with spacing)";
+                                }
+                            }
+
+                            lblSliderValue.innerHTML = "Offset = " + offsetValue + "m " + offsetDirection;
+                            $("#lblOffsetDescription").text(offsetDescription);
+                        }
+                    }
+                }
+
+                $("#btnConvertToSections").click(function (e) {
+                    $("#divTrackBuilderStep1").hide();
+                    $("#divTrackBuilderStep2").fadeIn();
+                });
+
+                $("#btnTrackConnectorFirst").click(function (e) {
+                    activeTrackConnector = 1;
+                    changeActiveTrackConnector();
+                });
+
+                $("#btnTrackConnectorBack").click(function (e) {
+                    if (activeTrackConnector > 1) {
+                        activeTrackConnector--;
+                        changeActiveTrackConnector();
+                    }
+                });
+
+                $("#btnTrackConnectorForward").click(function (e) {
+                    if (activeTrackConnector < trackConnectorCount) {
+                        activeTrackConnector++;
+                        changeActiveTrackConnector();
+                    }
+                });
+
+                $("#btnTrackConnectorLast").click(function (e) {
+                    activeTrackConnector = trackConnectorCount;
+                    changeActiveTrackConnector();
+                });
+
+                $("#btnTrackConnectorDelete").click(function (e) {
+                    $("#divDialogHeader").text("Delete Track Connector?");
+                    $("#divDialogBody").html("Are you sure you want to delete track connector " + activeTrackConnector + "?<br/><br/>Note: This track connector is in use so the track progression list will be re-generated.");
+                    $("#dlgConfirm").modal("show");
+                });
+
+                $("#lnkClearTrackProgressionList").click(function (e) {
+                    $("#divDialogHeader").text("Clear Track Progression List?");
+                    $("#divDialogBody").html("Are you sure you want to delete all traversals?");
+                    $("#dlgConfirm").modal("show");
+                });
+
+                $("#btnNavigateToLastTrackConnector").click(function (e) {
+                    if (traversalAdded) {
+                        activeTrackConnector = 2;
+                    } else {
+                        activeTrackConnector = trackConnectorCount;
+                    }
+                    changeActiveTrackConnector();
+                });
+
+                $("#btnAddTraversal3To2").click(function (e) {
+                    activeTrackConnector = 2;
+                    traversalAdded = true;
+                    $("#trFrom3To2").show();
+                    $("#lblDistance6").hide();
+                    $("#lblDistance10").show();
+                    $("#divAddTraversalValidStep2").show();
+                    $("#divAddTraversalValidStep1").hide();
+                    $("#lnkDeleteStep1").hide();
+                    $("#ico-navigate-track-connector").removeClass("fa-circle-3");
+                    $("#ico-navigate-track-connector").addClass("fa-circle-2");
+                    changeActiveTrackConnector();
+                });
+            });
         }
     };
     /* End Public Functions */
