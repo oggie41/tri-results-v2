09c78230c138d126bde90d2139aa919c4be5a4a1
David Ogle <dogle@bartecmunicipal.com>
2022-07-14 10:37:55 +0100

    Now calculating run pace based on distance rather than using GPS pace

--- TriResultsV2/Helpers/RunHelper.cs
+++ TriResultsV2/Helpers/RunHelper.cs
@@ -18,5 +18,41 @@ namespace TriResultsV2.Helpers
 
             return avgPaceFormatted;
         }
+
+        public static string GetRunPaceMinMi(double distance, DistanceUnit distanceUnit, TimeSpan totalTime)
+        {
+            double distanceInMiles = 0;
+            TimeSpan? paceTimeSpan = null;
+
+            if (distanceUnit == DistanceUnit.Miles)
+            {
+                if (distance == 13.1 || distance == 26.2)
+                {
+                    distanceInMiles = distance;
+                }
+            }
+            else if (distanceUnit == DistanceUnit.Kilometres)
+            {
+                if (distance == 5)
+                {
+                    distanceInMiles = 3.1;
+                }
+                else if (distance == 10)
+                {
+                    distanceInMiles = 6.2;
+                }
+            }
+
+            if (distanceInMiles > 0 && totalTime.TotalSeconds > 0)
+            {
+                var paceInSeconds = totalTime.TotalSeconds / distanceInMiles;
+                
+                paceTimeSpan = TimeSpan.FromSeconds(Math.Round(paceInSeconds, 2));
+            }
+
+            string formattedAvgPace = GetFormattedAvgPace(paceTimeSpan);
+
+            return formattedAvgPace;
+        }
     }
 }
